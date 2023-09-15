7c1f13e2c96ac7dbbe588160187f85e14ee7dcf2
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2023-09-15 20:14:19 -0300

    Update Exercício 1.java

--- Exercício 1.java
+++ Exercício 1.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	    
-	    Scanner read = new Scanner(System.in);
-	    float media, n1, n2;
-		  System.out.print("Informe o primeiro número: ");
+		Scanner read = new Scanner(System.in);
+		float media, n1, n2;
+		System.out.print("Informe o primeiro número: ");
 		    n1 = read.nextFloat();
-		  System.out.print("Informe o segundo número: ");
+		System.out.print("Informe o segundo número: ");
 		    n2 = read.nextFloat();
-		  media = (n1 + n2)/2;
-		  System.out.printf("A média aritmétrica dos números informados é igual a %.1f", media);
+		media = (n1 + n2)/2;
+		System.out.printf("A média aritmétrica dos números informados é igual a %.1f", media);
 	}
 }
