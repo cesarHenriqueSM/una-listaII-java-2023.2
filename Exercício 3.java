0baebdf59295f82c527d3a8e3eab23f4138aa0db
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2023-09-15 19:40:08 -0300

    Exercício 3.java

---
+++ Exercício 3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		
+		double temperatura_fahrenheit, temperatura_centigrados;
+		System.out.print("Informe a temperatura em Fahrenheit: ");
+		  temperatura_fahrenheit = read.nextDouble();
+		  temperatura_centigrados = (temperatura_fahrenheit - 32)/1.8;	
+		System.out.printf("A temperatura informada convertida em graus centígrados é igual a %.2f°C.", temperatura_centigrados);
