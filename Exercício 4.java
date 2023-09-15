c4a26fbcd4c0c298e52d29d880a47c9836dca878
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2023-09-15 19:43:43 -0300

    Update Exercício 4.java

--- Exercício 4.java
+++ Exercício 4.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-    double capital_aplicado;
+    		double capital_aplicado;
 		System.out.print("Informe o valor a ser aplicado: R$");
-		c  apital_aplicado = read.nextDouble();
+			capital_aplicado = read.nextDouble();
 		System.out.print("Informe o prazo de aplicação do capital em meses: ");
-		  int prazo = read.nextInt();
+			int prazo = read.nextInt();
 		System.out.print("Informe a taxa de juros mensal da aplicação: ");
-		  double taxa = read.nextDouble();
-		  double montante = capital_aplicado * Math.pow(1 + taxa, prazo);
+			double taxa = read.nextDouble();
+			double montante = capital_aplicado * Math.pow(1 + taxa, prazo);
 		System.out.printf("O montante final será de R$%.2f.", montante);
   }
 }
