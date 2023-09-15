94311be6ea90aa2f83e9ddea0ca80e48d98086ad
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2023-09-15 20:13:38 -0300

    Update Exercício 6.java

--- Exercício 6.java
+++ Exercício 6.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-      double numero, numero1;
-		  System.out.print("Digite os 2 números para cálculo do logaritmo do primeiro número informado na base pelo segundo: ");
-		    numero = read.nextDouble();
-		    numero1 = read.nextDouble();
-		  double resultado = Math.log(numero) / Math.log(numero1);
-		  System.out.printf("O logaritmo de %.1f na base %.1f é: %.1f", numero, numero1, resultado);
+	Scanner read = new Scanner(System.in);
+	double numero, numero1;
+	System.out.print("Digite os 2 números para cálculo do logaritmo do primeiro número informado na base pelo segundo: ");
+		numero = read.nextDouble();
+		numero1 = read.nextDouble();
+	double resultado = Math.log(numero) / Math.log(numero1);
+	System.out.printf("O logaritmo de %.1f na base %.1f é: %.1f", numero, numero1, resultado);
     }
 }  
