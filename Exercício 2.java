c8c5d9fa652102767562c3aa3a30b1815946990d
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2023-09-15 19:38:30 -0300

    Exercício 2.java

---
+++ Exercício 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		float valor_produto;
+		System.out.print("Informe o valor do produto: R$");
+		  valor_produto = read.nextFloat();
+		float acrescimo = (valor_produto/100)*10;
+		float valor_final = valor_produto + acrescimo;
+		System.out.printf("O valor informado acrescido é igual a R$%.2f", valor_final);
