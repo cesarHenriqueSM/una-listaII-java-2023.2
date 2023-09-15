5701b64d3e98523cc9d00425892899f138292c97
César Henrique <103151947+cesarHenriqueSM@users.noreply.github.com>
2023-09-15 20:12:41 -0300

    Update Exercício 3.java

--- Exercício 3.java
+++ Exercício 3.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-		
+
+		Scanner read = new Scanner(System.in);
 		double temperatura_fahrenheit, temperatura_centigrados;
 		System.out.print("Informe a temperatura em Fahrenheit: ");
 		  temperatura_fahrenheit = read.nextDouble();
